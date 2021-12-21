3634c2fce37b8abff9fd2df7354938ea90d148cb
fearm3 <ibraa1628@gmail.com>
2021-12-21 10:52:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+  
+
+
+  return <h2>tabs project setup</h2>;
+}
+
+export default App;
